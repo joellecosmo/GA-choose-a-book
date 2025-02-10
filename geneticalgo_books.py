f93a5c4469ecb67e04de06c4883d145b84f06753
Joelle Cosmo <131820548+joellecosmo@users.noreply.github.com>
2025-02-10 12:57:42 +0300

    added the file that includes the books

---
+++ geneticalgo_books.py
@@ -0,0 +1,42 @@
+knowledge_area = {
+    "history": "001",
+    "poetry": "010",
+    "education": "011",
+    "religion": "100",
+    "human development": "101",
+    "novel": "110",
+    "computer science": "111"
+}
+
+attractiveness = {1: "yes", 0: "no"}
+over_300_pages = {1: "yes", 0: "no"}
+important_for_my_project ={1: "yes", 0: "no"}
+author_importance = {
+    "unknown": "00",
+    "slightly famous": "01",
+    "famous": "10"
+}
+
+books = [
+    {"name": "The History of Ancient Civilizations", "author": "John Doe", "attributes": "00111110"},
+    {"name": "The Essential Poems of Rumi", "author": "Rumi", "attributes": "01010001"},
+    {"name": "Teaching and Learning in the Digital Age", "author": "Jane Smith", "attributes": "01111110"},
+    {"name": "The Essence of the Quran", "author": "Ibn Khaldun", "attributes": "10001110"},
+    {"name": "Atomic Habits", "author": "James Clear", "attributes": "10111110"},
+    {"name": "Pride and Prejudice", "author": "Jane Austen", "attributes": "11011001"},
+    {"name": "Introduction to Algorithms", "author": "Thomas H. Cormen", "attributes": "11111110"},
+    {"name": "A Brief History of Time", "author": "Stephen Hawking", "attributes": "00111110"},
+    {"name": "Leaves of Grass", "author": "Walt Whitman", "attributes": "01010010"},
+    {"name": "Educational Psychology", "author": "John Santrock", "attributes": "01110101"},
+    {"name": "The Bible as Literature", "author": "Northrop Frye", "attributes": "10010001"},
+    {"name": "How to Win Friends and Influence People", "author": "Dale Carnegie", "attributes": "10111010"},
+    {"name": "To Kill a Mockingbird", "author": "Harper Lee", "attributes": "11010010"},
+    {"name": "The Pragmatic Programmer", "author": "Andrew Hunt", "attributes": "11111101"},
+    {"name": "Guns, Germs, and Steel", "author": "Jared Diamond", "attributes": "00111010"},
+    {"name": "The Collected Poems of Emily Dickinson", "author": "Emily Dickinson", "attributes": "01010011"},
+    {"name": "Psychology of Learning for Instruction", "author": "Marcy Driscoll", "attributes": "01111101"},
+    {"name": "The Tao of Pooh", "author": "Benjamin Hoff", "attributes": "10010010"},
+    {"name": "The Power of Now", "author": "Eckhart Tolle", "attributes": "10110110"},
+    {"name": "The Catcher in the Rye", "author": "J.D. Salinger", "attributes": "11010101"},
+    {"name": "Design Patterns: Elements of Reusable Object-Oriented Software", "author": "Erich Gamma", "attributes": "11111100"}
+]
